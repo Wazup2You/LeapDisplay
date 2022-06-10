dbd55d28a5a7fe20a0be34ec0104d4dedbb72f74
Weifang <wazup2you@gmail.com>
2022-06-10 11:00:45 +0200

    Added clock and date

---
+++ src/js/classes/Date.js
@@ -0,0 +1,15 @@
+function currentDate() {
+
+
+
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.getElementById("date-container").innerText = today; 
+
+}
+
+currentDate();
